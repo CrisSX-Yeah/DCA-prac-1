a8199cc7eb9a4a7c52c48771d29866805622b561
CrisSX-Yeah <crissx0159sx@gmail.com>
2024-09-21 16:48:51 +0200

    1.1.3 full done

--- Practica 1/calculadora-versionado/src/test/java/estable/one_zero_x/one_zero_one/CalculadoraTest.java
+++ Practica 1/calculadora-versionado/src/test/java/estable/one_zero_x/one_zero_one/CalculadoraTest.java
@@ -1,6 +1,6 @@
 package estable.one_zero_x.one_zero_one;
 
-import estable.one_zero_x.one_zero_zero.Calculadora;
+import estable.one_zero_x.one_zero_one.Calculadora;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,26 +10,26 @@ public class CalculadoraTest {
 
     @Test
     public void testSumar() {
-        assertEquals(5, estable.one_zero_x.one_zero_zero.Calculadora.sumar(2, 3));
-        assertEquals(-1, estable.one_zero_x.one_zero_zero.Calculadora.sumar(2, -3));
+        assertEquals(5, Calculadora.sumar(2, 3));
+        assertEquals(-1, Calculadora.sumar(2, -3));
     }
 
     @Test
     public void testRestar() {
-        assertEquals(1, estable.one_zero_x.one_zero_zero.Calculadora.restar(3, 2));
-        assertEquals(5, estable.one_zero_x.one_zero_zero.Calculadora.restar(2, -3));
+        assertEquals(1, Calculadora.restar(3, 2));
+        assertEquals(5, Calculadora.restar(2, -3));
     }
 
     @Test
     public void testMultiplicar() {
-        assertEquals(6, estable.one_zero_x.one_zero_zero.Calculadora.multiplicar(2, 3));
-        assertEquals(-6, estable.one_zero_x.one_zero_zero.Calculadora.multiplicar(2, -3));
+        assertEquals(6, Calculadora.multiplicar(2, 3));
+        assertEquals(-6, Calculadora.multiplicar(2, -3));
     }
 
     @Test
     public void testDividir() {
-        assertEquals(2, estable.one_zero_x.one_zero_zero.Calculadora.dividir(6, 3));
-        assertThrows(ArithmeticException.class, () -> estable.one_zero_x.one_zero_zero.Calculadora.dividir(6, 0));
+        assertEquals(2, Calculadora.dividir(6, 3));
+        assertThrows(ArithmeticException.class, () -> Calculadora.dividir(6, 0));
     }
 
     @Test
